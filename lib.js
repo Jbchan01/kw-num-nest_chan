74f5e2dedd6dd1044feb26f421f3bcefc245c085
chan <cchhann01@gmail.com>
2024-11-04 16:06:47 +0900

    main

--- lib.js
+++ lib.js
@@ -1,12 +1,23 @@
 function avg(numbers) {
+    if (!Array.isArray(numbers) || numbers.length === 0) {
+        return "Insufficient parameter!";
+    }
     let s = numbers.reduce((prev, curr) => prev + curr, 0);
     return s / numbers.length;
 }
 function prime(num) {
-    return false;
+    for (let i =2; i< num; i++){
+        if(num %i ===0) return false;
+    }
+    return true;
 }
+
 function factorial(num) {
-    return -1;
+    let fac = 1;
+    for(let i = num;i>=2;i--){
+        fac *= i;
+    }
+    return fac;
 }
 module.exports = {
     avg,
--- lib.test.js
+++ lib.test.js
@@ -6,3 +6,15 @@ test("avg([3, 5, 7]) should be 5", () => {
 test("avg([-5, 5]) should be 0", () => {
     expect(lib.avg([-5, 5])).toBe(0);
 });
+test("prime(5) should be true", () => {
+    expect(lib.prime(5)).toBe(true);
+});
+test("prime(6) should be true", () => {
+    expect(lib.prime(6)).toBe(false);
+});
+test("factorial(5) should be true", () => {
+    expect(lib.factorial(5)).toBe(120);
+});
+test("factorial(5) should be true", () => {
+    expect(lib.factorial(6)).toBe(720);
+});
